3a7051d5d7760ffecb2d0d74870c2f41313ab928
Mark Bower <mark.bower@yale.edu>
2020-09-15 11:25:04 -0600

    Removed some more print statements

--- Analysis/meftools/R/mef_info.R
+++ Analysis/meftools/R/mef_info.R
@@ -13,25 +13,25 @@ mef_info <- function( args ) {
   password <- args[2]
 
   # Read the headerpwd
-  print( paste0( filename, ' ', password ) )
+#  print( paste0( filename, ' ', password ) )
   header = read_mef_header(c(filename,password))
 #  print( header )
   
   # Read the ToC
   # Use Rcpp to pass in 'index_data_offset' and 'number_of_index_entries', then
   # return the ToC.
-  print( 'Reading ToC' )
+#  print( 'Reading ToC' )
   variables <- c(filename,header$index_data_offset,header$number_of_index_entries)
-  print( paste0( variables ) )
+#  print( paste0( variables ) )
   ToC <- table_of_contents( variables )
-  print( 'Read ToC' )
+#  print( 'Read ToC' )
 
   # Read the discontnuities
   # Use Rcpp to pass in 'number_of_index_entries', then
   # return the discontinuity flags.
-  print( 'Reading discontinuities' )
+#  print( 'Reading discontinuities' )
   discontinuities <- get_discontinuities( c(filename,header$number_of_index_entries), ToC );
-  print( 'Read discontinuities' )
+#  print( 'Read discontinuities' )
   
   # Put the returnable values in a named list.
   value <- list( header=header, ToC=ToC, discontinuities=discontinuities );  
