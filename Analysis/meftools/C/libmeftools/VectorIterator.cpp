bae2ad2fdf79bfca059ea232c9ade28793c93321
markrbower <markrbower@yahoo.com>
2025-12-05 15:11:02 -0700

    Added VectorIterator.cpp

---
+++ Analysis/meftools/C/libmeftools/VectorIterator.cpp
@@ -0,0 +1,48 @@
+// From: https://softwarepatterns.com/cpp/iterator-software-pattern-cpp-example
+
+/* Usage:
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    Iterator<int>* iterator = new VectorIterator<int>(numbers);
+
+    while (iterator->hasNext()) {
+        std::cout << iterator->next() << " ";
+    }
+
+    delete iterator;
+
+    return 0;
+}
+*/
+
+
+#include <iostream>
+#include <vector>
+
+// Iterator interface
+template <typename T>
+class Iterator {
+public:
+    virtual T next() = 0;
+    virtual bool hasNext() = 0;
+};
+
+// Concrete Iterator for a vector
+template <typename T>
+class VectorIterator : public Iterator<T> {
+public:
+    VectorIterator(std::vector<T>& vec) : vector_(vec), index_(0) {}
+
+    T next() override {
+        return vector_[index_++];
+    }
+
+    bool hasNext() override {
+        return index_ < vector_.size();
+    }
+
+private:
+    std::vector<T>& vector_;
+    size_t index_;
+};
+
