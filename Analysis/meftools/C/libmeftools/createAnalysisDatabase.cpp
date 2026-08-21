a394f3405af0fbc3d76b539e209bbf4be76d4454
markrbower <markrbower@yahoo.com>
2026-08-21 11:31:49 -0600

    main_1_createDatabaseTables runs

--- Analysis/meftools/C/libmeftools/createAnalysisDatabase.cpp
+++ Analysis/meftools/C/libmeftools/createAnalysisDatabase.cpp
@@ -43,14 +43,38 @@ void createAnalysisDatabase( const char* name ) {
     	sprintf( queryStr, "drop table if exists subjects;" );
     	dba.runSQL( queryStr );
 	cout << "creating subjects table" << endl;
-    	dba.runSQL("create table subjects (dbIDsubject binary(16), name varchar(64), species varchar(64));" );
+    	dba.runSQL("create table subjects (dbIDsubject binary(16) PRIMARY KEY, name varchar(64), species varchar(64));" );
 	cout << "subjects table created" << endl;
 
 	cout << "Create Collections table" << endl;
 	// collection	dbIDcollection,    name, dbIDsubject , date, place, task
     	sprintf( queryStr, "drop table if exists collections;" );
     	dba.runSQL( queryStr );
-    	dba.runSQL("create table collections (dbIDcolletions binary(16), name varchar(64), dbIDsubject binary(16), date DATE, place varchar(64), task varchar(64) );" );
+    	dba.runSQL("create table collections (dbIDcolletion binary(16) PRIMARY KEY, name varchar(64), dbIDcollection binary(16), date DATE, place varchar(64), task varchar(64) );" );
+
+	cout << "Create Analyses table" << endl;
+	// analysis	dbIDanalysis,      name, description
+    	sprintf( queryStr, "drop table if exists analyses;" );
+    	dba.runSQL( queryStr );
+    	dba.runSQL("create table analyses (dbIDanalysis binary(16) PRIMARY KEY, name varchar(64), description varchar(128) );" );
+
+	cout << "Create Experiments table" << endl;
+	// experiment	dbIDexperiment, name, dbIDcollection, dbIDanalysis
+    	sprintf( queryStr, "drop table if exists experiments;" );
+    	dba.runSQL( queryStr );
+    	dba.runSQL("create table experiments (dbIDexperiment binary(16) PRIMARY KEY, name varchar(64), dbIDcollection binary(16), dbIDanalysis binary(16) );" );
+
+	cout << "Create Events table" << endl;
+	// events		dbIDevents,        name, dbIDexperiment, time, data, label
+    	sprintf( queryStr, "drop table if exists events;" );
+    	dba.runSQL( queryStr );
+    	dba.runSQL("create table events (dbIDevent binary(16) PRIMARY KEY, name varchar(64), dbIDexperiment binary(16), time BIGINT, data varchar(256), label varchar(32) );" );
+
+	cout << "Create metrics table" << endl;
+	// metrics		dbIDmetrics,       name, dbIDevents, value
+    	sprintf( queryStr, "drop table if exists metrics;" );
+    	dba.runSQL( queryStr );
+    	dba.runSQL("create table metrics (dbIDmetric binary(16) PRIMARY KEY, name varchar(64), dbIDevent binary(16), value varchar(64) );" );
 
 	cout << "Database tables created." << endl;
 }
